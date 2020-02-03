217f9f35ac0b206130333faeec45c31b5f0b146f
mustafaag <mustafaaga23@gmail.com>
2020-02-03 11:22:32 +0100

    Adding new todos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos'
+import AddTodo from './components/AddForm';
+
 
 class App extends Component {
   state = {
@@ -17,11 +19,20 @@ class App extends Component {
       todos : leftTodo
     })
   }
+
+  addTodo = (todo) =>{
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos:todos
+    })
+  }
   render (){
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's </h1>
         <Todos  todos={this.state.todos} deleteTodo = {this.deleteTodo}/>
+        <AddTodo  addTodo = {this.addTodo}/>
       </div>
     );
   }  
